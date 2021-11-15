673a8eeb1670b2a85d9eec539ca159714e925aa4
mapa95 <m.alves@j-alves.com>
2021-11-15 16:57:20 +0000

    Teste apagar

--- ES_Project_G7/src/test.java
+++ ES_Project_G7/src/test.java
@@ -5,7 +5,6 @@ public class test {
 		System.out.println("World");
 		System.out.println("Tiago");
 		System.out.println("Hugo");
-		System.out.println("Testaaaaoo do txiaguu");
 		System.out.println("Mara");
 	}
 
