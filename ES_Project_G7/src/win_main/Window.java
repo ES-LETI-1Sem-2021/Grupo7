944eb74d925fa8c01e86418349bbcaa9f9aeb4c0
santoshugo011 <hasss1@iscte-iul.pt>
2021-11-22 17:44:02 +0000

    window

--- ES_Project_G7/src/win_main/Window.java
+++ ES_Project_G7/src/win_main/Window.java
@@ -1,4 +1,37 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+
+
+
 package win_main;
+
+
+/**
+ *Create Window.
+ */
+
 public class Window {
+	public Window() {
+		createWindow();
+	}
+	
+	private void createWindow() {
+		//Create and set up the window. 
+		JFrame frame = new JFrame("Simple GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel textLabel = new JLabel("label",SwingConstants.CENTER); 
+		textLabel.setPreferredSize(new Dimension(300, 100));
+		frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+		//Display the window
+		frame.setLocationRelativeTo(null);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	
+	
+	
 	
 }
