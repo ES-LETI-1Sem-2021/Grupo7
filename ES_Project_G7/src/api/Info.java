d2989512a31940b078908524d3a5ee55e4238bd9
mapa95 <m.alves@j-alves.com>
2021-11-22 19:05:50 +0000

    Inicialização da classe Info.

--- ES_Project_G7/src/api/Info.java
+++ ES_Project_G7/src/api/Info.java
@@ -1,6 +1,42 @@
 package api;
 
+import java.io.IOException;
+
+import javax.swing.JPanel;
+import org.kohsuke.github.GitHub;
+import com.julienvey.trello.Trello;
 
 public class Info {
 
+	private JPanel content = new JPanel();
+	private GitHub gitMvn;
+	private Trello trelloMvn;
+
+	
+	public Info(GitHub gitMvn, Trello trelloMvn) {
+		this.gitMvn = gitMvn;
+		this.trelloMvn = trelloMvn;
+	}
+	
+	/**
+	 * Get repository from GitHub.
+	 */
+	private void connectGit (String login, String accessToken, String rep) throws IOException {
+		gitMvn.getUser(login);
+		gitMvn.getRepository(login+"/"+rep);
+	}
+
+	/**
+	 * Get GitHub API.
+	 */
+	public GitHub getGitHub() {
+		return gitMvn;
+	}
+
+	/**
+	 * Get Trello API.
+	 */
+	public Trello getTrello() {
+		return trelloMvn;
+	}
 }
