cbdd5b06e4b02270f740634a57628d880ad1a6a1
mapa95 <m.alves@j-alves.com>
2021-11-22 17:17:55 +0000

    Primeiras classes. Atualização das dependências (github e trello).

---
+++ ES_Project_G7/src/win_main/Start.java
@@ -0,0 +1,9 @@
+package win_main;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Window w = new Window();
+	}
+
+}
---
+++ ES_Project_G7/src/win_main/Window.java
@@ -0,0 +1,5 @@
+package win_main;
+
+public class Window {
+
+}
