bf42512b1a0462e45aaefd837680f1e5793ffee0
gamoe <gamoe@iscte-iul.pt>
2021-11-23 14:47:22 +0000

    Update Git Token

--- ES_Project_G7/src/api/Start.java
+++ ES_Project_G7/src/api/Start.java
@@ -4,19 +4,19 @@ import java.awt.EventQueue;
 
 public class Start {
 
-public static void main( String[] args )
-{
-	EventQueue.invokeLater(new Runnable() {
-		public void run() {
-			try {
-			Win j = new Win();
-				j.getFrame().setVisible(true);
-				
-				
-			} catch (Exception e) {
-				e.printStackTrace();
+	public static void main( String[] args )
+	{
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Win j = new Win();
+					j.getFrame().setVisible(true);
+
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		}
-	});	
-	
-}}
+		});	
+
+	}}
--- ES_Project_G7/src/api/Win.java
+++ ES_Project_G7/src/api/Win.java
@@ -1,55 +1,55 @@
-package api;
-
-
-
-
-
-	import java.awt.*;
-	import javax.swing.*;
-	public class Win{  
-		private JFrame frame;
-		private JTextField gitHubToken;
-		private JTextField trelloKey;
-		private JButton nextButton;
-		private JTextField trelloToken;
-		private JTextField trelloUser;
+package api;	
+import java.awt.*;
+import javax.swing.*;
+public class Win{  
+	private JFrame frame;
+	private JTextField gitToken;
+	private JTextField trelloKey;
+	private JButton nextButton;
+	private JTextField trelloToken;
+	private JTextField trelloUser;
 	// Create a simple GUI window 
-	
+
 	public Win() { createWindow();}
-	  
-		private void createWindow() {      
-			//Create and set up the window.      
-			JFrame frame = new JFrame("Simple GUI");      
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
-			JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER); 
-			textLabel.setPreferredSize(new Dimension(300, 100));   
-			frame.getContentPane().add(textLabel, BorderLayout.CENTER);    
-			//Display the window.
-			frame.setLocationRelativeTo(null);  
-			frame.pack();    
-			frame.setVisible(true);  
-			}  
-		public String getGitHubToken() {
-			return gitHubToken.getText();
-		}
 
-		public String getTrelloKey() {
-			return trelloKey.getText();
-		}
-
-		public String getTrelloToken() {
-			return trelloToken.getText();
-		}
-
-		public String getTrelloUser() {
-			return trelloUser.getText();
-		}
-
-		public Window getFrame() {
-			return frame;
-		}
-	
+	private void createWindow() {      
+		//Create and set up the window.      
+		JFrame frame = new JFrame("Window");      
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+		JLabel textLabel = new JLabel("Git Repository"); 
+		frame.setBounds(100, 100, 527, 403);
+		textLabel.setBounds(10, 11, 300, 37);
+		textLabel.setPreferredSize(new Dimension(300, 300));   
+		frame.getContentPane().add(textLabel); 
+		//Tokengithub
+		gitToken =new JTextField();
+		gitToken.setText("write here your github token");
+		gitToken.setBounds(20, 59, 404, 29);
+		frame.getContentPane().add(gitToken);
+		gitToken.setColumns(10);
+		//Display the window.
+		frame.setLocationRelativeTo(null);  
+		frame.pack();    
+		frame.setVisible(true);  
+	}  
+	public String getGitHubToken() {
+		return gitToken.getText();
 	}
-	
-	
 
+	public String getTrelloKey() {
+		return trelloKey.getText();
+	}
+
+	public String getTrelloToken() {
+		return trelloToken.getText();
+	}
+
+	public String getTrelloUser() {
+		return trelloUser.getText();
+	}
+
+	public Window getFrame() {
+		return frame;
+	}
+
+}
